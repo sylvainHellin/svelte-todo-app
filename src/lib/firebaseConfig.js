f2926dff292ab3725f43fb4385cf28984e0282a4
sylvain hellin <hellinsylvain@gmail.com>
2024-08-11 23:10:08 +0200

    adding firebaseconfig file

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,21 @@
+// import { VITE_apiKey, VITE_authDomain,  VITE_projectId, VITE_storageBucket, VITE_messagingSenderId, VITE_appId, VITE_measurementId} from '$env/static/private';
+
+// export const firebaseConfig = {
+//     apiKey: VITE_apiKey,
+//     authDomain: VITE_authDomain,
+//     projectId: VITE_projectId,
+//     storageBucket: VITE_storageBucket,
+//     messagingSenderId: VITE_messagingSenderId,
+//     appId: VITE_appId,
+//     measurementId: VITE_measurementId
+// };
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD2sMI19t9ohI6_LO0dxlIW1ygXWyo_K9c",
+    authDomain: "svelte-todo-app-sh.firebaseapp.com",
+    projectId: "svelte-todo-app-sh",
+    storageBucket: "svelte-todo-app-sh.appspot.com",
+    messagingSenderId: "272694091297",
+    appId: "1:272694091297:web:5ee74cbedccc9f00ae8d60",
+    measurementId: "G-81WSLBFJMJ"
+};
